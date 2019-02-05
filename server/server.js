b7e7d4220dca08096dfa4dcb622d9e089a58d34a
Jared Asmus <github@jaredasmus.com>
2019-02-04 20:39:13 -0700

    Add GET todos route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,16 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+        });
+    }, (e) => {
+        res.status(400).send(err);
+    })
+});
+
+app.get('/todos', (req, res) => {
 
 });
 
