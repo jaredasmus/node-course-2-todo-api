2672b83cda7dc7989e044b8f8670207639c1312d
Jared Asmus <github@jaredasmus.com>
2019-02-04 21:24:35 -0700

    added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 const app = express();
 
@@ -33,7 +34,20 @@ app.get('/todos', (req, res) => {
     })
 });
 
-app.get('/todos', (req, res) => {
+app.get('/todos/:id', (req, res) => {
+    if(!ObjectID.isValid(req.params.id)) {
+        return res.status(404).send("Invalid ID, try again.");
+    }
+
+    Todo.findById(req.params.id).then(todo => {
+
+        if(!todo) {
+            return res.status(404).send("Did not find a TODO with that id");
+        }
+        res.send({todo})
+    }).catch(err => {
+        res.status(400).send(err);
+    });
 
 });
 
